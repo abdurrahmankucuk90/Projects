0c2615a8437eec097330e0dce5a8593b1a6e80f4
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-06 10:38:21 +0200

    aaa

---
+++ CheckOutForm/price.js
@@ -0,0 +1,7 @@
+const buttondec1 = document.querySelector(".dec-1");
+const buttondec2 = document.querySelector(".dec-2");
+const buttoninc1 = document.querySelector(".inc-1");
+const buttoninc2 = document.querySelector(".inc-2");
+const piece1 = document.querySelector(".adet_inn1");
+const piece2 = document.querySelector(".adet_inn2");
+
