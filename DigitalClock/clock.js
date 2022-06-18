9eda6414e551990ead3a742c9af8410c47e2bdf0
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-18 15:43:35 +0200

    clockapp

---
+++ DigitalClock/clock.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+let saniye;
+let dakika;
+let saat;
+
+const clockTimer = setInterval(() => {
+  saat = new Date().getHours()
+  saat = saat < 10 ? "0" + saat : saat
+  dakika = new Date().getMinutes();
+  dakika = dakika < 10 ? "0" + dakika : dakika
+  saniye = new Date().getSeconds()
+  saniye = saniye < 10 ? "0" + saniye : saniye
+//   console.log(saat, ":", dakika, ":", saniye); 
+  container.innerHTML = `<p class="clock">${saat}:${dakika}:${saniye}</p>`;
+}, 1000);
+// clockTimer()
