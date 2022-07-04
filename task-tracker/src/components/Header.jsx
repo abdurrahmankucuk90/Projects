8e365c9742ec4e3d3134b3acca177499ff9453e5
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-07-04 22:11:27 +0200

    das

--- task-tracker/src/components/Header.jsx
+++ task-tracker/src/components/Header.jsx
@@ -1,31 +1,48 @@
-import AddTaskForm from "./AddTaskForm";
-import { useState } from "react";
+import AddTaskForm from './AddTaskForm';
+import { useState } from 'react';
 
-const Header = () => {
+const Header = ({ tasks, setTasks }) => {
   const [show, setShow] = useState(false);
   const [btnStyle, setBtnStyle] = useState({
-    name: "SHOW ADDTASK BAR",
-    bgColor: "purple",
+    name: 'SHOW ADD TASK BAR',
+    bgColor: 'purple',
   });
 
-  const handleShow = () => {
-    //*State fonksiyon bitmeden guncellenmez
-    //*React Stateleri async yontemi ile guncelliyor
-    if (show) {
-      setBtnStyle({ name: "SHOW ADDTASK BAR", bgColor: "purple" });
-    } else {
-      setBtnStyle({ name: "CLOSE ADDTASK BAR", bgColor: "red" });
-    }
+  //! React, default olarak state'leri hemen degistirmeyebilir.
+  //! Ekstra render'lari azaltmak icin state'leri toplu (batch) bir sekilde gunceller.
+  //! Bir event handler icerisindeki ardasik state'ler event bitiminde toplu bir
+  //! sekilde guncellenmis olur.State'lerin guncelenmesi gelis sirasina gorere yapilir.
+  //! Ayni event icerisinde birbirine bagli state'leri kullanirken buna dikkat etmek gerkir.
 
+  //? https://stackoverflow.com/questions/48563650/does-react-keep-the-order-for-state-updates
+
+  const handleShow = () => {
+    if (show) {
+      setBtnStyle({
+        name: 'SHOW ADD TASK BAR',
+        bgColor: 'purple',
+      });
+    } else {
+      setBtnStyle({
+        name: 'CLOSE ADD TASK BAR',
+        bgColor: 'red',
+      });
+    }
     setShow(!show);
   };
+  console.log(show);
+
   return (
     <header className="header">
       <h1>TASK TRACKER</h1>
-      <button className="btn" style={{backgroundColor:btnStyle.bgColor}} onClick={handleShow}>
+      <button
+        onClick={handleShow}
+        className="btn"
+        style={{ backgroundColor: btnStyle.bgColor }}
+      >
         {btnStyle.name}
       </button>
-      {show && <AddTaskForm />}
+      {show && <AddTaskForm tasks={tasks} setTasks={setTasks} />}
     </header>
   );
 };
--- task-tracker/src/pages/Home.jsx
+++ task-tracker/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
 import ShowTasks from "../components/ShowTasks";
+import { useState } from "react";
+import data from "../helper/starterData";
 
 const Home = () => {
+  const [tasks, setTasks] = useState(data);
+
   return (
     <div>
-      <Header />
+      <Header tasks={tasks} setTasks = {setTasks}/>
       <ShowTasks />
     </div>
   );
