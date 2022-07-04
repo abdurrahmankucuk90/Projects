5116725d54a1f1f80d1573c36a2105146b18aff5
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-07-04 21:40:36 +0200

    state

--- task-tracker/src/components/Header.jsx
+++ task-tracker/src/components/Header.jsx
@@ -1,5 +1,33 @@
+import AddTaskForm from "./AddTaskForm";
+import { useState } from "react";
+
 const Header = () => {
-  return <header>Header</header>;
+  const [show, setShow] = useState(false);
+  const [btnStyle, setBtnStyle] = useState({
+    name: "SHOW ADDTASK BAR",
+    bgColor: "purple",
+  });
+
+  const handleShow = () => {
+    //*State fonksiyon bitmeden guncellenmez
+    //*React Stateleri async yontemi ile guncelliyor
+    if (show) {
+      setBtnStyle({ name: "SHOW ADDTASK BAR", bgColor: "purple" });
+    } else {
+      setBtnStyle({ name: "CLOSE ADDTASK BAR", bgColor: "red" });
+    }
+
+    setShow(!show);
+  };
+  return (
+    <header className="header">
+      <h1>TASK TRACKER</h1>
+      <button className="btn" style={{backgroundColor:btnStyle.bgColor}} onClick={handleShow}>
+        {btnStyle.name}
+      </button>
+      {show && <AddTaskForm />}
+    </header>
+  );
 };
 
 export default Header;
